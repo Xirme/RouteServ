9c6ab43ab85bac09df04eb2711a8e8f98e1b4405
Keith Gadberry <me@kdude63.com>
2013-08-10 12:23:35 -0700

    Fixing .tracert

--- src/me/xir/irc/routeserv/commands/Tracert.java
+++ src/me/xir/irc/routeserv/commands/Tracert.java
@@ -1,18 +1,36 @@
 package me.xir.irc.routeserv.commands;
 
-import org.pircbotx.User;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import org.pircbotx.hooks.ListenerAdapter;
 import org.pircbotx.hooks.events.MessageEvent;
 
+@SuppressWarnings("rawtypes")
 public class Tracert extends ListenerAdapter {
+	Runtime rt = Runtime.getRuntime();
+	Process p;
+	InputStream out;
+	BufferedReader reader;
+	String s = null;
 	
-	public void onMessage(MessageEvent event) {
+	public void onMessage(MessageEvent event) throws IOException {
 		if (event.getMessage().startsWith(".tracert")) {
 			String[] arguments = event.getMessage().split(" ");
 			if(arguments.length == 2){
 				String host = "something";
+				p = rt.exec("traceroute " + host);
 				
 				event.getBot().sendMessage(event.getChannel(), "--BEGINNING TRACEROUTE TO " + host + " --");
+				
+				out = p.getInputStream();
+				reader = new BufferedReader(new InputStreamReader(out));
+				
+				while ((s = reader.readLine()) != null) {
+					event.getBot().sendMessage(event.getChannel(), s);
+				}
 			}
 		}
 	}
